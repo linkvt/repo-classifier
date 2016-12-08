1e8098552796383b6a03efd15eff8244cc857a42
Eduard Marbach <mail@eduard-marbach.de>
2016-12-08 20:40:11 +0100

    Rename Github and add method for returning repo for an URL

---
+++ GithubAuthentification.py
@@ -0,0 +1,29 @@
+import configparser
+
+from github import Github
+
+
+class GithubAuthentication:
+    def __init__(self):
+        config = configparser.ConfigParser()
+        config.read("github.conf")
+
+        fileUsername = config.get("DEFAULT", "username")
+        filePassword = config.get("DEFAULT", "password")
+        fileToken = config.get("DEFAULT", "token")
+
+        if not fileUsername and not fileToken:
+            print("Username and Token is not filled in github.conf file. Please input login data.")
+            raise LookupError("github.conf is not properly filled.")
+
+        if fileToken:
+            self.api = Github(fileToken)
+        else:
+            self.api = Github(fileUsername, filePassword)
+
+    def test(self):
+        for repo in self.api.get_user().get_repos():
+            print(repo.name)
+
+    def get_repo(self, url):
+        return self.api.get_repo(url)
