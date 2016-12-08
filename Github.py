56720636589826e4885567c3e2ff7a098239c2bb
Eduard Marbach <mail@eduard-marbach.de>
2016-12-08 19:18:15 +0100

    Add github login with token

--- Github.py
+++ Github.py
@@ -1,18 +1,24 @@
-from github import Github
 import configparser
 
+from github import Github
+
 config = configparser.ConfigParser()
-config.read('github.conf')
+config.read("github.conf")
 
 fileUsername = config.get("DEFAULT", "username")
 filePassword = config.get("DEFAULT", "password")
 fileToken = config.get("DEFAULT", "token")
 
-if not fileUsername:
-        print("Username is not filled in github.conf file. Please input login data.")
-        raise LookupError("github.conf is not properly filled.")
+if not fileUsername and not fileToken:
+    print("Username and Token is not filled in github.conf file. Please input login data.")
+    raise LookupError("github.conf is not properly filled.")
 
-g = Github(fileUsername, filePassword)
+githubApi = None
 
-for repo in g.get_user().get_repos():
+if fileToken:
+    githubApi = Github(fileToken)
+else:
+    githubApi = Github(fileUsername, filePassword)
+
+for repo in githubApi.get_user().get_repos():
     print(repo.name)
