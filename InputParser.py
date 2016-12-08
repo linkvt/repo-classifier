899a3680c015e59af2beed4a87761b0be9e21506
Eduard Marbach <mail@eduard-marbach.de>
2016-12-08 20:39:21 +0100

    Rename Input and return only substring owner/repo

--- InputParser.py
+++ InputParser.py
@@ -1,4 +1,4 @@
-class Input:
+class InputParser:
     def __init__(self, path):
         self.path = path
 
@@ -6,4 +6,5 @@ class Input:
         """Parse the file given the path and return a list of strings containing each line"""
         f = open(self.path)
         text = f.read()
-        return text.split("\n")
+        return [path[19:] for path in text.split("\n")]
+
