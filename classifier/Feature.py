3e035ccbf641e44c4ba94480926c45959d9a6398
Eduard Marbach <mail@eduard-marbach.de>
2016-12-10 16:44:56 +0100

    Overwrite representation of feature

--- classifier/Feature.py
+++ classifier/Feature.py
@@ -1,4 +1,10 @@
 class Feature:
     def __init__(self, name, value):
-        self._name = name
-        self._value = value
+        self.name = name
+        self.value = value
+
+    def __str__(self):
+        return 'Feature[' + self.name + ', ' + str(self.value) + ']'
+
+    def __repr__(self):
+        return self.__str__()
