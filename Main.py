8c9d3eebbd56db155f36676665ed52166c0f6dba
Eduard Marbach <mail@eduard-marbach.de>
2016-12-10 16:49:27 +0100

    Use extraction pipeline

--- Main.py
+++ Main.py
@@ -1,9 +1,9 @@
 import argparse
 
-from classifier.FeatureExtractor import FeatureExtractor
+from classifier.DecisionTreeClassifier import DecisionTreeClassifier
 from classifier.GithubAuthentification import GithubAuthentification
 from classifier.InputParser import InputParser
-from classifier.DecisionTreeClassifier import DecisionTreeClassifier
+from classifier.feature_extraction.FeatureExtractionPipeline import FeatureExtractionPipeline
 
 parser = argparse.ArgumentParser(description='Program which analyses github repositories into categories.')
 parser.add_argument('-f', '--file', dest="filepath", help='The file location of the input file', nargs='?', required=True,
@@ -26,10 +26,10 @@ if args.train:
     for (url, current_label) in zip(splitted_urls, labels):
         current_repo = github_connection.get_repo(url)
         print('<Testing> Read repo name:{} with label {}'.format( current_repo.name, current_label))
-        features = FeatureExtractor(current_repo).extract_features()
+        features = FeatureExtractionPipeline(current_repo).extract_features()
         print('Extracted features: ', features)
         samples.append(features)
 
     clf.fit(samples, labels)
-    predicted_label = clf.predict([[5]])
-    print("Predict label for repo with 5 forks: {}".format(predicted_label))
+    predicted_label = clf.predict_with_values([[5, 1]])
+    print('Prediction: ', predicted_label)
