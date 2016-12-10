327df3dc5c5f5a1f8b77a154f3030ccbacc561d5
Eduard Marbach <mail@eduard-marbach.de>
2016-12-10 16:49:17 +0100

    Map list of features to values for scikit classifier

--- classifier/DecisionTreeClassifier.py
+++ classifier/DecisionTreeClassifier.py
@@ -6,7 +6,32 @@ class DecisionTreeClassifier():
         self.clf = tree.DecisionTreeClassifier()
 
     def fit(self, samples, labels):
-        self.clf = self.clf.fit(samples, labels)
+        """
+        samples eq. -> [[Feature1, Feature2], [Feature1, Feature2]]
+        :param samples:
+        :param labels:
+        :return:
+        """
+        self.clf = self.clf.fit(self.__map_input__(samples), labels)
 
     def predict(self, samples):
+        """
+        The sample input contains only the necessary values for scikit input
+        :param samples:
+        :return:
+        """
+        return self.clf.predict(self.__map_input__(samples))
+
+    def predict_with_values(self, samples):
         return self.clf.predict(samples)
+
+    def __map_input__(self, samples):
+        """
+        We need a specific input for the scikit classifier
+        :param samples:
+        :return:
+        """
+        mapped_samples = []
+        for sample in samples:
+            mapped_samples.append([feature.value for feature in sample])
+        return mapped_samples
