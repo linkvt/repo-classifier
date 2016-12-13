e3bfea93dd5b11ff230275cce2e3841779e1297e
Eduard Marbach <mail@eduard-marbach.de>
2016-12-13 21:46:58 +0100

    Remove category hierarchy

--- classifier/feature_extraction/FeatureCategory.py
+++ classifier/feature_extraction/FeatureCategory.py
@@ -1,4 +1,5 @@
-import itertools
+import abc
+from itertools import chain
 
 from classifier import Feature
 from classifier.feature_extraction.FeatureExtractor import FeatureExtractor
@@ -12,33 +13,12 @@ class FeatureCategory:
 
     def __init__(self, repo):
         self.repo = repo
-        self.categories = self._get_categories()
-        self.weight = self._get_weight()
-        self.features = list(itertools.chain.from_iterable(
+        self.features = list(chain.from_iterable(
             (extractor.extract_features() for extractor in self._get_feature_extractors())))
-        self.validate()
-
-    def _get_weight(self) -> float:
-        """
-        Value between 0 and 1
-        :return:
-        """
-        return 1
 
     def extract(self) -> [Feature]:
-        if self.categories:
-            return list(itertools.chain.from_iterable((category.extract() for category in self.categories)))
         return self.features
 
-    def validate(self):
-        if self.categories and self.features:
-            raise AssertionError('Categories and features are both filled.')
-        if not self.categories and not self.features:
-            raise AssertionError('Categories and features are both empty')
-
-
-    def _get_categories(self):
-        return []
-
+    @abc.abstractmethod
     def _get_feature_extractors(self) -> [FeatureExtractor]:
-        return []
+        raise AssertionError('Must be overridden')
--- classifier/feature_extraction/FeatureExtractionPipeline.py
+++ classifier/feature_extraction/FeatureExtractionPipeline.py
@@ -1,4 +1,5 @@
-import itertools
+from itertools import chain
+
 from github.Repository import Repository
 
 from classifier.Feature import Feature
@@ -15,8 +16,7 @@ class FeatureExtractionPipeline:
         self._repo = repo
 
     def extract_features(self) -> [Feature]:
-        return list(
-            itertools.chain.from_iterable((category.extract() for category in self.extract_features_in_categories())))
+        return list(chain.from_iterable((category.extract() for category in self.extract_features_in_categories())))
 
     def extract_features_in_categories(self) -> [FeatureCategory]:
         return [category(self._repo) for category in CATEGORIES]
