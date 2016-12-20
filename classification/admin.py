da9fa9477163e20d9605800d56ee632aa9f64013
Vincent Link <mail@linkvt.de>
2016-12-20 18:38:53 +0100

    Add import and export to repository admin view

--- classification/admin.py
+++ classification/admin.py
@@ -1,5 +1,21 @@
 from django.contrib import admin
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin
 
 from classification.models import Repository
 
-admin.site.register(Repository)
+
+class RepositoryResource(resources.ModelResource):
+    class Meta:
+        model = Repository
+        exclude = ('id',)
+        export_order = ('url', 'category')
+        import_id_fields = ('url',)
+        skip_unchanged = True
+
+
+class RepositoryAdmin(ImportExportModelAdmin):
+    resource_class = RepositoryResource
+
+
+admin.site.register(Repository, RepositoryAdmin)
