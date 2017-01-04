b377a179bd1aa0030e4bd20847017f6abe6b6037
paul.kuznecov <mail@paul-kuznecov.de>
2017-01-04 18:05:19 +0100

    Set name parameter in feature contructor

--- classification/feature_extraction/dev.py
+++ classification/feature_extraction/dev.py
@@ -13,7 +13,7 @@ class HasBuildFileExtractor(FeatureExtractor):
                    'requirements.txt', 'manifest.json', 'Package.swift']
 
     def _init_features(self):
-        self.features = [Feature('Has build file')]
+        self.features = [Feature(name='Has build file')]
 
     def _extract(self):
         try:
