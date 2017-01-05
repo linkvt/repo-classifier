cd2bf4713f5a4ab3b2a43217fa134f477bad9cf3
Vincent Link <mail@linkvt.de>
2017-01-06 00:54:36 +0100

    Implement file extension extractor
    See #29

--- classification/feature_extraction/misc.py
+++ classification/feature_extraction/misc.py
@@ -1,4 +1,8 @@
 """Contains extractors that belong to no specific category but can be used in multiple categories."""
+import abc
+import os
+from collections import defaultdict
+
 from github import GithubException
 
 from classification.feature_extraction.FeatureExtractor import FeatureExtractor
@@ -42,3 +46,46 @@ class FileNameExtractor(FeatureExtractor):
                 feature.value = 1
             else:
                 feature.value = 0
+
+
+class FileExtensionExtractor(FeatureExtractor):
+    def _init_features(self):
+        self._extension_to_size_features = {}
+        self._extension_to_count_features = {}
+
+        for ext in self.extensions_to_check:
+            to_count_feature = Feature(name='Share of extension "{}" by count'.format(ext))
+            self._extension_to_count_features[ext] = to_count_feature
+            to_size_feature = Feature(name='Share of extension "{}" by size'.format(ext))
+            self._extension_to_size_features[ext] = to_size_feature
+            self.features.extend([to_count_feature, to_size_feature])
+
+    def _extract(self):
+        try:
+            git_tree = self.api_repo.get_git_tree(self.api_repo.default_branch, recursive=True)
+        except GithubException:
+            git_tree = None
+        items = git_tree.tree if git_tree else []
+        files = [item for item in items if item.type == 'blob']
+
+        extensions_to_count = defaultdict(int)
+        extensions_to_size = defaultdict(int)
+        for file in files:
+            _, extension = os.path.splitext(file.path)
+            extension = extension.lower()
+            extensions_to_count[extension] += 1
+            extensions_to_size[extension] += file.size
+
+        total_count = sum(extensions_to_count.values())
+        total_size = sum(extensions_to_size.values())
+
+        for ext in self.extensions_to_check:
+            self._extension_to_count_features[ext].value = extensions_to_count[ext] / total_count
+            self._extension_to_size_features[ext].value = extensions_to_size[ext] / total_size
+
+    @abc.abstractproperty
+    def extensions_to_check(self) -> [str]:
+        """
+        :return: lower case file extensions prefixed by a dot like '.java'
+        """
+        raise NotImplementedError()
