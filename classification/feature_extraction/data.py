ce18a43835906d911c920cdb0a7e993ef55168b9
paul.kuznecov <mail@paul-kuznecov.de>
2017-01-12 08:54:45 +0100

    Add geojson to DATAFileExtensionExtractor

--- classification/feature_extraction/data.py
+++ classification/feature_extraction/data.py
@@ -13,4 +13,4 @@ class DATAFileExtensionExtractor(FileExtensionExtractor):
 
     @property
     def extensions_to_check(self) -> typing.Set[str]:
-        return {'.json', '.xml', '.csv', '.yml', '.txt', '.sql', '.xls', '.xlsx', '.zip'}
+        return {'.json', '.geojson', '.xml', '.csv', '.yml', '.txt', '.sql', '.xls', '.xlsx', '.zip'}
