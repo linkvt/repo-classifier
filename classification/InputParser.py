e78311f74eb6e9ca4745e2983411f976bf76b01c
paul.kuznecov <mail@paul-kuznecov.de>
2017-01-12 08:22:35 +0100

    Change InputParser to work with spaces and commas

--- classification/InputParser.py
+++ classification/InputParser.py
@@ -6,8 +6,12 @@ class InputParser:
     def parse(self):
         lines = [line.rstrip() for line in self.text.split('\n') if line]
         if self.train:
-            del lines[0]
-            split_text = [line.split(",") for line in lines]
+            separator = " "
+            # Input with commas
+            if len(lines[0].split(",")) == 2:
+                separator = ","
+                del lines[0]
+            split_text = [line.split(separator) for line in lines]
             repo_ids = [t[0] for t in split_text]
             labels = [t[1] for t in split_text]
             return repo_ids, labels
