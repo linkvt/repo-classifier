bdd03b1025c19a1af85018aeb21795b405cceb0f
paul.kuznecov <mail@paul-kuznecov.de>
2017-01-16 17:48:07 +0100

    Improve classification algorithms

--- classification/algorithm/algorithms.py
+++ classification/algorithm/algorithms.py
@@ -3,7 +3,8 @@ from sklearn import neighbors
 from sklearn import neural_network
 from sklearn import svm
 from sklearn.externals import joblib
-from sklearn.feature_selection import SelectKBest
+from sklearn.feature_selection import SelectPercentile
+from sklearn.feature_selection import mutual_info_classif
 from sklearn.model_selection import GridSearchCV
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import MinMaxScaler
@@ -13,9 +14,9 @@ class Classifier:
     model_file_name = 'model.pkl'
 
     def __init__(self, clf, params, name):
-        self.clf = Pipeline([('kbest', SelectKBest()), ('clf', clf)])
-        params['kbest__k'] = [30, 40, 50, 60, 70, 'all']
-        self.clf = GridSearchCV(self.clf, param_grid=params)
+        self.clf = Pipeline([('select', SelectPercentile(score_func=mutual_info_classif, percentile=70)), ('clf', clf)])
+        params['select__percentile'] = [70, 75, 80, 85, 90]
+        self.clf = GridSearchCV(self.clf, param_grid=params, scoring='f1_macro')
         self.name = name
         self.scaler = MinMaxScaler()
 
@@ -27,8 +28,21 @@ class Classifier:
         :return:
         """
         feature_vectors = self._map_input(samples)
+        feature_names = [feature.name for feature in samples[0]]
         scaled_vectors = self.scaler.fit_transform(feature_vectors)
         self.clf = self.clf.fit(scaled_vectors, labels)
+
+        if hasattr(self.clf.best_estimator_.named_steps['clf'], 'feature_importances_'):
+            feature_indices = self.clf.best_estimator_.named_steps['select'].get_support(indices=True)
+            selected_features = [feature_names[i] for i in feature_indices]
+            feature_importances = self.clf.best_estimator_.named_steps['clf'].feature_importances_
+            feature_tuples = [(feature, importance) for feature, importance in
+                              zip(selected_features, feature_importances)]
+            sorted_features = sorted(feature_tuples, key=lambda t: t[1], reverse=True)
+
+            for name, importance in sorted_features:
+                print("Feature {}: {}".format(name, importance))
+
         print('Best Params:')
         print(self.clf.best_params_)
 
@@ -68,14 +82,16 @@ class Classifier:
 
 class RandomForestClassifier(Classifier):
     def __init__(self):
-        params = dict(clf__n_estimators=[20, 25, 30, 40, 50])
-        super().__init__(ensemble.RandomForestClassifier(), params, 'RandomForestClassifier')
+        params = dict(clf__n_estimators=[60, 80, 100], clf__min_samples_split=[2], clf__max_depth=[30, 40])
+        class_weights = {'DEV': 1, 'WEB': 2, 'DATA': 4, 'DOCS': 4, 'EDU': 15, 'HW': 5, 'OTHER': 10}
+        super().__init__(ensemble.RandomForestClassifier(class_weight=class_weights), params, 'RandomForestClassifier')
 
 
 class ExtraTreesClassifier(Classifier):
     def __init__(self):
-        params = dict(clf__n_estimators=[20, 25, 30, 40, 50])
-        super().__init__(ensemble.ExtraTreesClassifier(), params, 'ExtraTreesClassifier')
+        params = dict(clf__n_estimators=[60, 80], clf__min_samples_split=[2], clf__max_depth=[30, 40])
+        class_weights = {'DEV': 1, 'WEB': 2, 'DATA': 4, 'DOCS': 4, 'EDU': 15, 'HW': 5, 'OTHER': 10}
+        super().__init__(ensemble.ExtraTreesClassifier(class_weight=class_weights), params, 'ExtraTreesClassifier')
 
 
 class KNeighborsClassifier(Classifier):
@@ -92,5 +108,5 @@ class SVMClassifier(Classifier):
 
 class MLPClassifier(Classifier):
     def __init__(self):
-        params = dict(clf__hidden_layer_sizes=[(100,), (120,), (140,)], clf__solver=['lbfgs', 'adam', 'sgd'])
+        params = dict(clf__hidden_layer_sizes=[(50,), (70,), (100,)], clf__solver=['lbfgs', 'adam', 'sgd'])
         super().__init__(neural_network.MLPClassifier(), params, 'MLPClassifier')
